c81084398e4834b8b594088822f10848be0717df
cdean <cdean@qualcomm.com>
2021-01-19 10:41:49 -0800

    Added reverse lookup feature. Renamed output file to results.csv.

--- lookup_hosts.py
+++ lookup_hosts.py
@@ -4,7 +4,7 @@ import time
 from argparse import ArgumentParser
 
 def main(args):
-    with open('hostnames.csv', 'w') as csvfile:
+    with open('results.csv', 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
 
         with open(args.hosts, 'r') as hostnames:
@@ -13,21 +13,33 @@ def main(args):
             ip = []
 
             for host in hostnames:
-                try:
-                    hostname, aliaslist, ip = socket.gethostbyname_ex(host.strip())
-                    csvwriter.writerow([hostname, aliaslist, ip[0]])
-                    print(hostname)
-                except socket.gaierror:
-                    ip = 'Unable to resolve'
-                    csvwriter.writerow([host, aliaslist, ip])
-                except socket.herror as e:
-                    print(e)
+                if args.ip:
+                    try:
+                        hostname, aliaslist, ip = socket.gethostbyaddr(host.strip())
+                        csvwriter.writerow([hostname, aliaslist, ip[0]])
+                        print(hostname)
+                    except socket.gaierror:
+                        ip = 'Unable to resolve'
+                        csvwriter.writerow([host, aliaslist, ip])
+                    except socket.herror as e:
+                        print(e)
+                else:
+                    try:
+                        hostname, aliaslist, ip = socket.gethostbyname_ex(host.strip())
+                        csvwriter.writerow([hostname, aliaslist, ip[0]])
+                        print(hostname)
+                    except socket.gaierror:
+                        ip = 'Unable to resolve'
+                        csvwriter.writerow([host, aliaslist, ip])
+                    except socket.herror as e:
+                        print(e)
 
 if __name__ == '__main__':
 
     # Parse command-line arguments
     parser = ArgumentParser()
     parser.add_argument('hosts', help='Name of text file that contains single column of hostnames to be looked up.')
+    parser.add_argument('-i', '--ip', action='store_true', help="Do a reverse lookup on IP addresses instead of hostnames.")
 
     args = parser.parse_args()
 
